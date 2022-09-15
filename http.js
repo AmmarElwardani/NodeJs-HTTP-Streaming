34c197f603bcefd24ca8d83b2b562cf7583ccbc6
Ammar <ammarelw@gmail.com>
2022-09-15 17:10:40 +0100

    added uploading videos

--- http.js
+++ http.js
@@ -1,31 +1,60 @@
 const { createServer } = require("http");
-const { stat, createReadStream } = require("fs");
+const { stat, createReadStream,  createWriteStream } = require("fs");
 const { promisify } = require("util");
+const { send } = require("process");
+const multiparty = require("multiparty");
+
 
 const fileName = "./video.mp4";
 const fileInfo = promisify(stat);
+const sendOGVideo = async (req, res) => {
+  const { size } = await fileInfo(fileName);
+  const range = req.headers.range;
+  if (range) {
+    let [start, end] = range.replace(/bytes=/, "").split("-");
+    start = parseInt(start, 10);
+    end = end ? parseInt(end, 10) : size - 1;
 
-createServer(async (req, res) => {
-    const {size} = await fileInfo(fileName); 
-    const range = req.headers.range;
-    if(range){
-      let [start, end] = range.replace(/bytes=/, '').split('-');
-      start = parseInt(start, 10);
-      end = end ? parseInt(end, 10) : size-1;
-  
-      res.writeHead(206, {
-        'Content-Range': `bytes ${start}-${end}/${size}`,
-        'Accept-Ranges': 'bytes',
-        'Content-Length': (start-end) + 1,
-        'Content-Type': 'video/mp4'
-      })
-  
-      createReadStream(fileName, {start, end}).pipe(res);
-    }else{
-      res.writeHead(200, {
-        'Content-Length': size,
-        'Content-Type': 'video/mp4'
-      });  
-      createReadStream(fileName).pipe(res);
+    res.writeHead(206, {
+      "Content-Range": `bytes ${start}-${end}/${size}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": start - end + 1,
+      "Content-Type": "video/mp4",
+    });
+
+    createReadStream(fileName, { start, end }).pipe(res);
+  } else {
+    res.writeHead(200, {
+      "Content-Length": size,
+      "Content-Type": "video/mp4",
+    });
+    createReadStream(fileName).pipe(res);
+  }
+};
+
+
+createServer((req, res) => {
+    if(req.method === "POST"){
+       let form = new multiparty.Form();
+        form.on('part', (part)=>{
+            part
+            .pipe(createWriteStream(`./test/${part.filename}`))
+            .on('close', ()=> {
+                res.writeHead(200, {'Content-Type': 'text/html'})
+                res.end(`<h1>File Uploaded: ${part.filename}</h1>`)
+            })
+        })
+       form.parse(req);
     }
+  else if (req.url === "/og") {
+    sendOGVideo(req, res);
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.end(`
+      <form enctype="multipart/form-data" method="POST" action="/">
+        <input type="file" name="upload-file">
+        <button>Upload File</button>
+      </form>
+    `)
+  }
 }).listen(5050, () => console.log("Server Running on port 5050"));
